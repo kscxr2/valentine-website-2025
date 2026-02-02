018fb7c20c8c5be3706123f282d8d6475f156e1c
kscxr2 <sahdevkavyesh@gmail.com>
2026-02-01 22:19:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! I'm the luckiest boyfriend in the world! 🎉💝💖💝💓",
+        message: "I LOVE YOU SO MUCH MY LOVE! SO MANY HUGGIS AND KISSIS FOR YOU :)",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
